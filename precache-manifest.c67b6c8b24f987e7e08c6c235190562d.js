d498a363703e8ae1b8035b30f2a37f5cfa67498e
litlifesoftware <56931590+litlifesoftware@users.noreply.github.com>
2020-11-03 20:57:36 +0100

    New website
    Entirely new website

---
+++ precache-manifest.c67b6c8b24f987e7e08c6c235190562d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b925235f7393be48ca965318dfa3bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea70b9f1b4863ca241f6",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "74f0b3b2ec06997fa72c",
+    "url": "/static/css/main.f7efd9ee.chunk.css"
+  },
+  {
+    "revision": "ea70b9f1b4863ca241f6",
+    "url": "/static/js/2.b47be59a.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/static/js/2.b47be59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f0b3b2ec06997fa72c",
+    "url": "/static/js/main.b475d722.chunk.js"
+  },
+  {
+    "revision": "6c47a8e05ae85c1fb000",
+    "url": "/static/js/runtime-main.084a8dea.js"
+  },
+  {
+    "revision": "e3242149669bebf6afc4b0b5d22ec6ae",
+    "url": "/static/media/Montserrat-Black.e3242149.ttf"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/static/media/Montserrat-Bold.a3b387c9.ttf"
+  },
+  {
+    "revision": "e375c6fe9bbeadb38d4911107e480378",
+    "url": "/static/media/Montserrat-ExtraBold.e375c6fe.ttf"
+  },
+  {
+    "revision": "94603128108f9cf9b26626564265c1d0",
+    "url": "/static/media/Montserrat-ExtraLight.94603128.ttf"
+  },
+  {
+    "revision": "2f354053d1c9c36f533ebd226698bfa5",
+    "url": "/static/media/Montserrat-Light.2f354053.ttf"
+  },
+  {
+    "revision": "0098f804fc2d06af52650e0b8ed3390c",
+    "url": "/static/media/Montserrat-Medium.0098f804.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "bbcd5bbb5993808a92df01a5dfef77cd",
+    "url": "/static/media/Montserrat-SemiBold.bbcd5bbb.ttf"
+  },
+  {
+    "revision": "b50f5872459e8e17eaffd5d57a2b2f8e",
+    "url": "/static/media/Montserrat-Thin.b50f5872.ttf"
+  },
+  {
+    "revision": "b4d0b8fcf812b5d9984a7ce94d6f4742",
+    "url": "/static/media/Remaining_Lifetime_Promo_Poster-01.b4d0b8fc.png"
+  },
+  {
+    "revision": "74c26c2d4213b1d6e35ac84e61ee180c",
+    "url": "/static/media/Remaining_Lifetime_Smartphone_Mockup_Screenshot_Clean-01.74c26c2d.png"
+  },
+  {
+    "revision": "7fd7f222addb23e3adbfa2b7acb74df8",
+    "url": "/static/media/irid-film-club-logo-grey-background-1-918x918.7fd7f222.png"
+  },
+  {
+    "revision": "0913769d1e6ad655820370d4afef02f9",
+    "url": "/static/media/irid-film-club-mockup-poster-2-1080x1080.0913769d.png"
+  },
+  {
+    "revision": "d0cde9c99f95e7c2c9296341580d8e6b",
+    "url": "/static/media/lit-life-logo-gradient-background-soft-brush-widescreen-2-1920x1080.d0cde9c9.png"
+  },
+  {
+    "revision": "106f8819bb6efe1b5a9f74efa9d915b6",
+    "url": "/static/media/lit_life_roadmap_history_of_me-01.106f8819.png"
+  },
+  {
+    "revision": "06df8e136e3d4af5580bfff845a6e5e4",
+    "url": "/static/media/placeholder-image-1-904x904.06df8e13.png"
+  }
+]);
